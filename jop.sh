1ef297082ce11fdaaf8622b49974b7b3a5e6557d
Jop Bakker <joppie.b@live.nl>
2018-05-18 11:54:43 +0200

    Adding git script

---
+++ jop.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+# Script gemaakt door: Jop Bakker - 359423 voor de les Linux Services aan de Hanzehogeschool Groningen.
+# Script om de Linux-Services repo te downloaden, pullen of pushen
+
+#Download the Linux-Services repo
+download () {
+	echo "Downloading the Linux-Services repo"
+	cd ~/Documents/git
+	git clone https://github.com/J-P-Bakker/Linux-Services.git
+	echo "Finnished downloading the Linux-Services repo"
+}
+
+#Pull (updates) from git
+pull () {
+	echo "Pulling the Linux-Services repo"
+	cd ~/Documents/git/Linux-Services
+	git pull
+	echo "Finnished puling Linux-Services repo"
+}
+
+#Commit and push changes to git
+push () {
+	echo "Commiting and pushing to the Linux-Services repo"
+	cd ~/Documents/git/Linux-Services
+	git add *
+	read -p "Commit message: " MESSAGE
+	git commit -m "$MESSAGE"
+	git push
+	echo "Finnished comminting to the Linux-Services repo"
+}
+
+if declare -f "$1" > /dev/null
+then
+  # call the function
+  "$@"
+else
+  # Return error
+  echo "'$1' is not a known function name" >&2
+  exit 1
+fi
