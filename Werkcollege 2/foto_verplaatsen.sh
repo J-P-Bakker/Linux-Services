054f3d2bee1297506751f1f9b5a2752909ff6a99
J-P-Bakker <joppie.b@live.nl>
2018-05-31 03:39:38 -0700

    Foto_sorteren.sh completed

--- Werkcollege 2/foto_verplaatsen.sh
+++ Werkcollege 2/foto_verplaatsen.sh
@@ -24,4 +24,37 @@ do
 	read -p "Sorteren op WEEK of MAAND: " FOTO_SORTEREN
 done
 
-echo "We gaan de foto's uit de map: $FOTO_LOCATIE sorteren op $FOTO_SORTEREN"
+
+for i in "$FOTO_LOCATIE"/*
+do
+	if test -f "$i"; then
+		filename="${i##*/}"
+		creationdate=$(ls -l --time-style='+%d-%m-%y' "$i" | awk '{print $6}')
+		if [ "$FOTO_SORTEREN" = "WEEK" ]; then
+			weeknum=$(date --date="$creationdate" +"%V")
+			num="Week $weeknum"
+		elif [ "$FOTO_SORTEREN" = "MAAND" ]; then
+			monthnum=$(date --date="$creationdate" +"%m")
+			num="Month $monthnum"
+		fi
+		
+		if [ ! -d "$FOTO_LOCATIE/$num" ]; then
+			mkdir "$FOTO_LOCATIE/$num"
+		fi
+		
+		cp "$i" "$FOTO_LOCATIE/$num/$filename"
+
+		originalhash=$(sudo md5sum "$i" | cut -d " " -f1)
+		movedhash=$(sudo md5sum "$FOTO_LOCATIE/$num/$filename" | cut -d " " -f1)
+		
+		if [ "$originalhash" = "$movedhash" ]; then
+			echo "INFO ($filename): Hashes are identical."
+			echo "INFO ($filename): Proceeding to remove original file."
+			sudo rm "$i"
+		else
+			echo "ERROR ($filename): Hashes do not match."
+			echo "ERROR ($filename): Proceeding to remove copied file."
+			sudo rm "$FOTO_LOCATIE/$num/$filename"
+		fi
+	fi
+done
